d4cfa0114d8c77da8e2acedb409d3b9e879f4a4f
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-09-02 14:25:04 -0500

    Pase a produccion

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+# class Config(object):
+#     SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@localhost:5432/bdcarsales"
+#     SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = "postgresql://admin:admin@localhost:5432/bdcarsales"
+    SQLALCHEMY_DATABASE_URI = "postgresql://bdcarsales_kh3u_user:jg9SUJJ1dz93eYCozm4SbYAYd7l6xT29@dpg-cjpof8lhe99c73amv0eg-a.oregon-postgres.render.com:5432/bdcarsales_kh3u"
     SQLALCHEMY_TRACK_MODIFICATIONS = True
