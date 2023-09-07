84fda86de8f3696bf02cb7a79be4ff0f28f8cbd1
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-09-07 16:04:54 -0500

    otros cors

--- app.py
+++ app.py
@@ -3,12 +3,6 @@ from app import app,db
 with app.app_context():
     db.create_all()
 
-@app.after_request
-def after_request(response):
-  response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-  return response    
 
 
 if __name__=='__main__':
